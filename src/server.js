ac7652be41e3a3c9b6dc2c9627ee89f61e3605e4
Rafael Luis <rafaeluisx@gmail.com>
2024-11-07 01:03:19 -0300

    feat: add initial endpoints

--- src/server.js
+++ src/server.js
@@ -1,7 +1,20 @@
 import http from 'node:http';
 
  const server = http.createServer((request, response)=> {
-    return response.end("Hello World");
+   const method = request.method;
+   const url = request.url;
+
+   if (method == 'GET' && url == '/users')
+   {
+      return response.end('Listagem de usuário');
+   }
+
+   if (method == 'POST' && url == '/users')
+   {
+      return response.end('Criação de usuário');
+   }
+   
+   return response.end("Hello World");
  });
 
  server.listen(3333);
