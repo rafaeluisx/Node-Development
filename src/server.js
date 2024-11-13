15ba4636801bc59dd22a7f8b0cf0d72109da088f
Rafael Luis <rafaeluisx@gmail.com>
2024-11-12 23:11:28 -0300

    feat: Using my own JSON from a POST method in insomnia

--- src/server.js
+++ src/server.js
@@ -2,9 +2,20 @@ import http from "node:http";
 
 const users = [];
 
-const server = http.createServer((request, response) => {
-  const method = request.method;
-  const url = request.url;
+const server = http.createServer(async (request, response) => {
+  const { method, url} = request;
+  const buffers = [];
+
+  try {
+
+    for await (const chunk of request){
+      buffers.push(chunk);
+    }
+    request.body = JSON.parse(Buffer.concat(buffers).toString());
+    
+  } catch (error){
+    request.body = null;
+  }
 
   if (method == "GET" && url == "/users") {
     return response
@@ -13,14 +24,17 @@ const server = http.createServer((request, response) => {
   }
 
   if (method == "POST" && url == "/users") {
+
+    const { name, email } = request.body;
+
     users.push({
       id: 1,
-      name: "John Doe",
-      email: "johndoe@example.com",
+      name: name,
+      email: email
     });
+
     return response.writeHead(201).end();
   }
-
   return response.writeHead(404).end();
 });
 
