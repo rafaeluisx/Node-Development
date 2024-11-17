3535a45e730f6371feb50cb2a08bccd285cff777
Rafael Luis <rafaeluisx@gmail.com>
2024-11-16 22:52:11 -0300

    feat: persisting data in a json local file

--- middlewares/database.js
+++ middlewares/database.js
@@ -1,5 +1,25 @@
+import fs from 'node:fs/promises';
+
+const dataBasePath = new URL('../db.json', import.meta.url);
+
 export class Database{
     #database = {};
+
+    constructor(){
+        fs.readFile(dataBasePath, 'utf8')
+        .then(data => {
+            this.#database = JSON.parse(data);
+        })
+        .catch(() => {
+            this.#persist();
+        });
+
+    }
+
+    #persist(){
+        fs.writeFile(dataBasePath, JSON.stringify(this.#database));
+    }
+    
 
     select(table){
         const data = this.#database[table] ?? [];
@@ -12,6 +32,8 @@ export class Database{
         }else{
             this.#database[table] = [data];
         }
+
+        this.#persist();
         return data;
     }
 }
