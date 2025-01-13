b58236273b3c04302d8e4a3cc842d626a42c083b
Rafael Luis <rafaeluisx@gmail.com>
2025-01-12 22:28:27 -0300

    feat: add route path builder

---
+++ utils/build-route-path.js
@@ -0,0 +1,5 @@
+export function buildRoutePath(path){
+    const routeParametersRegex = /:([a-zA-Z]+)/g;
+    const pathWithParams = path.replaceAll(routeParametersRegex, '([a-z0-9\-_]+)')
+    return path;
+}
