ce1b04e5cc527b632974bd6317b5fcee7fc8076b
Rafael Luis <rafaeluisx@gmail.com>
2025-01-13 23:43:54 -0300

    feat: including routes params

--- utils/build-route-path.js
+++ utils/build-route-path.js
@@ -1,5 +1,6 @@
 export function buildRoutePath(path){
     const routeParametersRegex = /:([a-zA-Z]+)/g;
-    const pathWithParams = path.replaceAll(routeParametersRegex, '([a-z0-9\-_]+)')
-    return path;
+    const pathWithParams = path.replaceAll(routeParametersRegex, '(?<$1>[a-z0-9\-_]+)');
+    const pathRegex = new RegExp(`^${pathWithParams}`);
+    return pathRegex;
 }
