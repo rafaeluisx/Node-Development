b6341440c820da1934d22e49393975b1c747925b
Rafael Luis <rafaeluisx@gmail.com>
2025-01-21 16:21:16 -0300

    feat: add delete route

--- src/server.js
+++ src/server.js
@@ -16,7 +16,9 @@ const server = http.createServer(async (request, response) => {
   if (route){
 
     const routeParams = request.url.match(route.path);
-    console.log(routeParams);
+    
+    request.params = { ...routeParams.groups};
+
     return route.handler(request, response);
   }
 
