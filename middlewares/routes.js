9f1f30972774bb535ff4288749963ca52dfa210e
Rafael Luis <rafaeluisx@gmail.com>
2025-01-22 13:48:27 -0300

    feat: add put route

--- middlewares/routes.js
+++ middlewares/routes.js
@@ -39,5 +39,21 @@ export const routes = [
 
             return response.writeHead(204).end();
         }
+    },
+    {
+        method: 'PUT',
+        path: buildRoutePath('/users/:id'),
+        handler: (request, response) => {
+
+            const {id} = request.params;
+            const {name, email} = request.body;
+            database.update('users', id, {
+                id,
+                name,
+                email,
+            });
+
+            return response.writeHead(200).end();
+        }
     }
 ];
